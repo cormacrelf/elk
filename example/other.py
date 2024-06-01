981dbd5c336bb4e66e99ffc695267cd570dbe80f
Cormac Relf <web@cormacrelf.net>
2024-06-02 01:34:09 +1000

    Add polars

--- example/other.py
+++ example/other.py
@@ -1,3 +1,13 @@
 import numpy as np
+import polars as pl
 
-print(np.arange(5))
+num_rows = 5000
+rng = np.random.default_rng(seed=7)
+
+buildings_data = {
+    "sqft": rng.exponential(scale=1000, size=num_rows),
+    "year": rng.integers(low=1995, high=2023, size=num_rows),
+    "building_type": rng.choice(["A", "B", "C"], size=num_rows),
+}
+buildings = pl.DataFrame(buildings_data)
+print(buildings)
